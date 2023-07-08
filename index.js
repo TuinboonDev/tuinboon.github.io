bc7bff22227c70287f83a518e44f443661dbb420
TuinboonDev <117291710+TuinboonDev@users.noreply.github.com>
2023-07-08 22:47:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+function rotate(i) {
+    
+}
+
+window.onload = function(){
+    for (i = 0; i<10; i++) {
+        
+        setTimeout(document.getElementById("wave").style.transform = "rotate("+1*20+ "deg)", 1000)
+    }
+};
+*/
